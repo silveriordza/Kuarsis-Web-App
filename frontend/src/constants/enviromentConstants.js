517ff87573019da9ff848c3e8fa8c0f8e1b94171
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarsis.com>
2023-05-12 08:03:19 -0400

    Standarized the environmentConstants.js file

--- frontend/src/constants/enviromentConstants.js
+++ frontend/src/constants/enviromentConstants.js
@@ -2,10 +2,17 @@
 
 
 
-//Change the BACKEND_ENDPOINT to point to the desired endpoint for the target environment.
+///Change the BACKEND_ENDPOINT to point to the desired endpoint for the target environment.
 //In case I want to test the BACKEND_ENDPOINT in the local machine then I can change it to 'http://localhost:5000'
 //In case I want to test the BACKEND_ENDPOINT in the AWS enviornment, then I can change it to 'https://ikhmic2rag.execute-api.us-east-1.amazonaws.com/dev'
+//In case I want to test the BACKEND_ENDPOINT in the AWS PIXANDEV dev enviornment, then I can change it to 'https://r8chx2099g.execute-api.us-east-1.amazonaws.com/pixandev'
+//In case I want to test the BACKEND_ENDPOINT in the AWS PIXAN prod enviornment, then I can change it to 'https://7p1h5lrqd5.execute-api.us-east-1.amazonaws.com/pixan'
 //This BACKEND_ENDPOPINT constant will be referenced across all actions invoking backend services, and I can also use it to switch to DEV, QA or PROD environments later.
+//In case I want to test in AWS environment I have to change the KUARSIS_PUBLIC_STATIC_FOLDER to 'https://www.kuarsis.com/images'
+//In case I want to test in AWS PIXANDEV environment I have to change the KUARSIS_PUBLIC_STATIC_FOLDER to 'https://pixandev.kuarsis.com/images'
+//In case I want to test in AWS PIXANDEV environment I have to change the KUARSIS_PUBLIC_STATIC_FOLDER to 'https://pixan.kuarsis.com/images'
+//In case I want to test in localhost environment I have to change the KUARSIS_PUBLIC_STATIC_FOLDER to 'http://localhost:3000/images'
+
 
 // LOCALHOST DEV VARIABLES
 /*
@@ -15,38 +22,37 @@ export const KUARSIS_BANNER_MAIN_LOGO = '/KuarsisLogo256px.png'
 export const KUARSIS_PUBLIC_BUCKET_URL =
   'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
 */
-/*  "proxy": "http://localhost:5000", */
-/*
-"proxy": "https://ikhmic2rag.execute-api.us-east-1.amazonaws.com/dev", 
-*/
 
-//AWS DEV ENVIRONMENT VARIABLES
+//CONSTANTS FOR DEV.KUARSIS.COM ENVIRONMENT
 export const BACKEND_ENDPOINT = 'https://ikhmic2rag.execute-api.us-east-1.amazonaws.com/dev'
 export const KUARSIS_PUBLIC_STATIC_FOLDER = 'https://dev.kuarsis.com/images'
 export const KUARSIS_BANNER_MAIN_LOGO = 'KuarsisLogo256px.png'
 export const KUARSIS_PUBLIC_BUCKET_URL = 'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
 
-/*PayPal Accounts and Data for Testing:
-
-PERSONAL (Represents the customer part of the transactions):
-sb-ru3fa8654408@personal.example.com pass: 12345678
-Name: John Doe, Phone 4082268073, Account Id: BTQ4ZAYWN2PYY, Country: US 
-
-BUSINESS (Represents the business part of the transactions)
-sb-grklv8654311@business.example.com pass: 12345678
-Name: John Doe, Phone 4084925537, Account Id: 6DL3EUVM82UDS
-
-ACTUAL BUSINESS ACCOUNT where my test transactions are going: 
-sb-myqv48599323@business.example.com pass: 12345678
-
-Use the Paypal Sandbox account to check the test transactions in paypal.
-
-Paypal Sandbox:
-Test Accounts https://developer.paypal.com/developer/accounts
-Papal Sandbox Test Site: https://www.sandbox.paypal.com/
-Paypal Sandbox > accounts page: https://developer.paypal.com/developer/accounts
-
-Paypal Live site: https://www.paypal.com/?_ga=1.174395558.565967115.1654411204
-
-
+//START
+//CONSTANTS FOR PIXANDEV.KUARSIS.COM (DEV) ENVIRONMENT
+/*
+export const BACKEND_ENDPOINT =
+  'https://r8chx2099g.execute-api.us-east-1.amazonaws.com/pixandev'
+export const KUARSIS_PUBLIC_STATIC_FOLDER =
+  'https://pixandev.kuarsis.com/images'
+export const KUARSIS_BANNER_MAIN_LOGO = 'PixanLogo256px.png'
+export const KUARSIS_PUBLIC_BUCKET_URL =
+  'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
 */
+
+//CONSTANTS FOR PIXAN.KUARSIS.COM (PROD) ENVIRONMENT
+/*
+export const BACKEND_ENDPOINT =
+  'https://7p1h5lrqd5.execute-api.us-east-1.amazonaws.com/pixan'
+export const KUARSIS_PUBLIC_STATIC_FOLDER = 'https://pixan.kuarsis.com/images'
+export const KUARSIS_BANNER_MAIN_LOGO = 'PixanLogo256px.png'
+export const KUARSIS_PUBLIC_BUCKET_URL =
+  'https://kuarsis-products-s3-public.s3.amazonaws.com/'
+*/
+
+/*
+For information about dev and prod users for testing, consult the "Setps to setup Kuarsis Web Page for a new GIT repository.docx" 
+document at the root of Kuarsis folder
+*/
+
