52e0ac82ce3a6a0a107d1189be819b871dff44c8
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2023-05-28 15:12:32 -0400

    removing turnsshon.sh since it only turned on ssh-agent on the context of the bash and not in the context of the terminal, it kept asking me passphrase on the terminal after running turnsshon.sh, hence no use for this bash, deleting it.

--- turnsshon.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Turning on ssh-agent"
-eval $(ssh-agent)
-echo "ssh-agent is now on"
-echo "requesting ssh passphrase please provide"
-ssh-add
-echo "adding ssh passphrase to ssh-agent completed"
