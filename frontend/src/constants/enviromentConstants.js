6dfdc4f97b2409944a4d809ff6c56f8d3070447b
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2023-06-17 22:22:32 -0400

    Updating environment constats to point to ArtPixanDev in AWS;

--- frontend/src/constants/enviromentConstants.js
+++ frontend/src/constants/enviromentConstants.js
@@ -1,16 +1,16 @@
 // LOCALHOST DEV VARIABLES
-
+/*
 export const LOG_LEVEL = 1
 export const BACKEND_ENDPOINT = 'http://localhost:5000'
 export const KUARSIS_PUBLIC_STATIC_FOLDER = 'http://localhost:3000/images'
 export const KUARSIS_BANNER_MAIN_LOGO = '/ArtPixanLogo256px.png'
 export const KUARSIS_PUBLIC_BUCKET_URL =
   'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
-
+*/
 
 //CONSTANTS FOR ARTPIXANDEV.KUARXIS.COM (DEV) ENVIRONMENT
-/*
-export const LOG_LEVEL = 0
+
+export const LOG_LEVEL = 1
 export const BACKEND_ENDPOINT =
   'https://o3dzma966j.execute-api.us-east-1.amazonaws.com/kuarxbedev'
 export const KUARSIS_PUBLIC_STATIC_FOLDER =
@@ -19,7 +19,6 @@ export const KUARSIS_BANNER_MAIN_LOGO = 'ArtPixanLogo256px.png'
 export const KUARSIS_PUBLIC_BUCKET_URL =
   'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
 
-*/
 //CONSTANTS FOR ARTPIXAN.KUARXIS.COM (PROD) ENVIRONMENT
 /*
 export const LOG_LEVEL = 0
