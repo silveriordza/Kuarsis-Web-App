bdce7d21c1b545a7c71cc550f42129c440a717d2
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2023-07-11 18:44:50 -0400

    modifying environmentConstants

--- frontend/src/constants/enviromentConstants.js
+++ frontend/src/constants/enviromentConstants.js
@@ -1,5 +1,5 @@
 // LOCALHOST DEV VARIABLES
-/*
+
 export const LOG_LEVEL = 1
 export const CURRENT_VERSION = `v1.0.0.6-2023-07-01-15:25`
 export const BACKEND_ENDPOINT = 'http://localhost:5000'
@@ -7,11 +7,11 @@ export const KUARSIS_PUBLIC_STATIC_FOLDER = 'http://localhost:3000/images'
 export const KUARSIS_BANNER_MAIN_LOGO = '/ArtPixanLogo256px.png'
 export const KUARSIS_PUBLIC_BUCKET_URL =
   'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
-*/
+
 
 //CONSTANTS FOR ARTPIXANDEV.KUARXIS.COM (DEV) ENVIRONMENT
-
-export const LOG_LEVEL = 0
+/*
+export const LOG_LEVEL = 1
 export const CURRENT_VERSION = `v1.0.0.6-2023-07-09-10:42`
 export const BACKEND_ENDPOINT =
   'https://o3dzma966j.execute-api.us-east-1.amazonaws.com/kuarxbedev'
@@ -20,7 +20,7 @@ export const KUARSIS_PUBLIC_STATIC_FOLDER =
 export const KUARSIS_BANNER_MAIN_LOGO = 'ArtPixanLogo256px.png'
 export const KUARSIS_PUBLIC_BUCKET_URL =
   'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
-
+*/
 
 //CONSTANTS FOR ARTPIXAN.KUARXIS.COM (PROD) ENVIRONMENT
 /*
