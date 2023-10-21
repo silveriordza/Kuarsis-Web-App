53edb377ca4a143ca9acf5195024aa0f904c8bcf
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2023-10-21 14:42:58 -0400

    Just other commit

--- frontend/src/screens/UploadSurveyAnswers.js
+++ frontend/src/screens/UploadSurveyAnswers.js
@@ -24,9 +24,12 @@ import {surveysConfigurations} from '../surveysConfigurations'
  
 
 const UploadSurveyAnswers = ({ match, history }) => {
+  const [myCSV, setmyCSV] = useState(false)
+
   const [nombreDeArchivo, setnombreDeArchivo] = useState('')
 
   const [uploading, setUploading] = useState(false)
+  
   const [fileContents, setfileContents] = useState(null)
   const [fileContentsReady, setfileContentsReady] = useState(false)
 
@@ -40,168 +43,159 @@ const UploadSurveyAnswers = ({ match, history }) => {
   const dispatch = useDispatch()
 
 const processCSVData = () => {
-  objLogSettings.sourceFilename = 'UploadSurveyAnswers.js'
-  objLogSettings.sourceFunction = 'processCSVData'
-  LogThis(objLogSettings, `START`)
-  if (fileContents) {
-
-    const rows = fileContents.split('\n');
-    const rowsReales = fileContentsReales.split('\n')
-    rowsReales.shift()
-    rowsReales.shift()
-    LogThis(objLogSettings, `rowsReales=${rowsReales}; rowsReales.lenght=${rowsReales.length}`)
-    let Row1 = rows[0];
-    let Row2 = rows[1];
-    console.log(Row1)
-    console.log(Row2)
-    
-
-    console.log("ROW 1 CLEANUP START")
-    const headersRow1 = rowCleaner(Row1).split(',');
-    console.log("ROW 1 CLEANUP END")
-    console.log("ROW 2 CLEANUP START")
-    const headersRow2 = rowCleaner(Row2).split(',');
-    console.log("ROW 2 CLEANUP END")
-    console.log(headersRow1)
-    console.log(headersRow2)
-    const finalHeaders = [];
-
-    surveysConfigurations.forEach((survey) => {
-      survey.subSurvey.forEach((subSurvey) => {
-        subSurvey.headerParserModificators.forEach(hpm => {
-          switch (hpm.modificator){
-            case SRV_BLANK_CONCAT_2NDROW_PREVIOUS_HEADER:
-              {
-                let currentValue = ''; 
-                let previousNonEmpty = ''
-              
-              for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
-                currentValue = headersRow1[col];
-                if(currentValue == '')
+  return new Promise((resolve, reject) => {
+    objLogSettings.sourceFilename = 'UploadSurveyAnswers.js'
+    objLogSettings.sourceFunction = 'processCSVData'
+    LogThis(objLogSettings, `START`)
+    if (fileContents) {
+  
+      const rows = fileContents.split('\n');
+      const rowsReales = fileContentsReales.split('\n')
+      rowsReales.shift()
+      rowsReales.shift()
+      LogThis(objLogSettings, `rowsReales=${rowsReales}; rowsReales.lenght=${rowsReales.length}`)
+      let Row1 = rows[0];
+      let Row2 = rows[1];
+      console.log(Row1)
+      console.log(Row2)
+      
+  
+      console.log("ROW 1 CLEANUP START")
+      const headersRow1 = rowCleaner(Row1).split(',');
+      console.log("ROW 1 CLEANUP END")
+      console.log("ROW 2 CLEANUP START")
+      const headersRow2 = rowCleaner(Row2).split(',');
+      console.log("ROW 2 CLEANUP END")
+      console.log(headersRow1)
+      console.log(headersRow2)
+      const finalHeaders = [];
+  
+      surveysConfigurations.forEach((survey) => {
+        survey.subSurvey.forEach((subSurvey) => {
+          subSurvey.headerParserModificators.forEach(hpm => {
+            switch (hpm.modificator){
+              case SRV_BLANK_CONCAT_2NDROW_PREVIOUS_HEADER:
                 {
-
-                  if(previousNonEmpty != ''){
-                    let concatValue = previousNonEmpty + headersRow2[col]
-                    finalHeaders.push(subSurvey.subSurveyName + '_' + concatValue)
-                  } else {
-                    for (let backCol = col-1; backCol>=0; col--)
-                    {
-                      let backColVal = headersRow1[backCol]
-                      if(backColVal!='')
-                      {
-                        previousNonEmpty = backColVal
-                        break
-                      }
-                    }
-                    if(previousNonEmpty == ''){
-                    throw new Error(`Header incorrect format on column: ${col}`)
-                  }
-                  }         
-                } else {
-                  console.log(`previousNonEmpty = ${previousNonEmpty}`)
-                  previousNonEmpty = headersRow1[col]
-                  finalHeaders.push(subSurvey.subSurveyName + '_' + headersRow1[col])
-                }
+                  let currentValue = ''; 
+                  let previousNonEmpty = ''
                 
-              }
-            }
-              break
-            case SRV_SECOND_ROW_HAS_HEADERS:
-              for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
-                finalHeaders.push(subSurvey.subSurveyName + '_' + headersRow2[col])
-              }
-              break
-            case SRV_CONCAT_1STROW_HEADER_2NDROW_HEADER:
-            { 
-              let previousValue = ''
-
-              if (headersRow1[hpm.startColumn] == ''){
-                throw new Error(`Header incorrect format on column: ${hpm.startColumn}; headersRow1[hpm.startColumn]=${headersRow1[hpm.startColumn]}`)
-              } 
-              
-              for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
-                if (headersRow1[col] != '' && headersRow1[col].charCodeAt(0)!=13)
-                {
-                  if(col==524){
-                    console.log(`PREVIOUS VALUE LAST ROW previousValue=${previousValue}`)
-                  }
-                  previousValue = headersRow1[col]
-                  if(col==524){
-                    const character = headersRow1[col]
-                    const asciiCode = character.charCodeAt(0)
-                    console.log(`NEW VALUE LAST ROW previousValue=${previousValue}; headersRow1[col]="${headersRow1[col]}"; asciiCode=${asciiCode}`)
-                  }
-                }
-               
-                finalHeaders.push(subSurvey.subSurveyName + '_' + previousValue + ' ' + headersRow2[col])
-              }
-            }
-              break
-            case SRV_FIRST_ROW_HAS_HEADERS:
-              { 
                 for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
-                  finalHeaders.push(subSurvey.subSurveyName + '_' + headersRow1[col])
+                  currentValue = headersRow1[col];
+                  if(currentValue == '')
+                  {
+  
+                    if(previousNonEmpty != ''){
+                      let concatValue = previousNonEmpty + headersRow2[col]
+                      finalHeaders.push(subSurvey.subSurveyName + '_' + concatValue)
+                    } else {
+                      for (let backCol = col-1; backCol>=0; col--)
+                      {
+                        let backColVal = headersRow1[backCol]
+                        if(backColVal!='')
+                        {
+                          previousNonEmpty = backColVal
+                          break
+                        }
+                      }
+                      if(previousNonEmpty == ''){
+                      throw new Error(`Header incorrect format on column: ${col}`)
+                    }
+                    }         
+                  } else {
+                    console.log(`previousNonEmpty = ${previousNonEmpty}`)
+                    previousNonEmpty = headersRow1[col]
+                    finalHeaders.push(subSurvey.subSurveyName + '_' + headersRow1[col])
+                  }
+                  
                 }
               }
-              break
-          }
+                break
+              case SRV_SECOND_ROW_HAS_HEADERS:
+                for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
+                  finalHeaders.push(subSurvey.subSurveyName + '_' + headersRow2[col])
+                }
+                break
+              case SRV_CONCAT_1STROW_HEADER_2NDROW_HEADER:
+              { 
+                let previousValue = ''
+  
+                if (headersRow1[hpm.startColumn] == ''){
+                  throw new Error(`Header incorrect format on column: ${hpm.startColumn}; headersRow1[hpm.startColumn]=${headersRow1[hpm.startColumn]}`)
+                } 
+                
+                for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
+                  if (headersRow1[col] != '' && headersRow1[col].charCodeAt(0)!=13)
+                  {
+                    if(col==524){
+                      console.log(`PREVIOUS VALUE LAST ROW previousValue=${previousValue}`)
+                    }
+                    previousValue = headersRow1[col]
+                    if(col==524){
+                      const character = headersRow1[col]
+                      const asciiCode = character.charCodeAt(0)
+                      console.log(`NEW VALUE LAST ROW previousValue=${previousValue}; headersRow1[col]="${headersRow1[col]}"; asciiCode=${asciiCode}`)
+                    }
+                  }
+                 
+                  finalHeaders.push(subSurvey.subSurveyName + '_' + previousValue + ' ' + headersRow2[col])
+                }
+              }
+                break
+              case SRV_FIRST_ROW_HAS_HEADERS:
+                { 
+                  for (let col = hpm.startColumn; col <= hpm.endColumn; col++) {
+                    finalHeaders.push(subSurvey.subSurveyName + '_' + headersRow1[col])
+                  }
+                }
+                break
+            }
+          })
         })
       })
-    })
-    
-    console.log(`final headers`)
-    console.log(finalHeaders)
-    const results = [];
-    for (let i = 2; i < rows.length; i++) {
-        const rowData = rows[i].split(',');
-        const rowObject = [];
-
-        for (let j = 0; j < finalHeaders.length; j++) {
-          console.log(`results j=${j}; finalHeaders[j]=${finalHeaders[j]}; rowData[j]=${rowData[j]}`)
-          
-          rowObject.push({header: finalHeaders[j], data: rowData[j]})
+      
+      console.log(`final headers`)
+      console.log(finalHeaders)
+      const results = [];
+      for (let i = 2; i < rows.length; i++) {
+          const rowData = rows[i].split(',');
+          const rowObject = [];
+  
+          for (let j = 0; j < finalHeaders.length; j++) {
+            console.log(`results j=${j}; finalHeaders[j]=${finalHeaders[j]}; rowData[j]=${rowData[j]}`)
+            
+            rowObject.push({header: finalHeaders[j], data: rowData[j]})
+          }
+           
+          results.push(rowObject);
+          console.log(`results`) 
+          console.log(results)
         }
-         
-        results.push(rowObject);
-        console.log(`results`) 
+  
+        console.log(`results are`)
         console.log(results)
-      }
-
-      console.log(`results are`)
-      console.log(results)
-      
-      const dataRows = [...rows]
-      console.log(`rows`)
-      console.log(rows)
-      console.log(`dataRows`)
-      console.log(dataRows)
-      dataRows.shift()
-      dataRows.shift()
-      //dataRows.pop()
-      console.log(`dataRows 2nd removed`)
-      console.log(dataRows)
-      const surveyCSVData = convertSurveyJsonToCSV(finalHeaders, dataRows, rowsReales)
-      console.log(`surveyCSVData`)
-      console.log(surveyCSVData)
-      saveStringAsCSV(surveyCSVData, `${new Date().getTime()/1000}_data.csv`)
+        
+        const dataRows = [...rows]
+        console.log(`rows`)
+        console.log(rows)
+        console.log(`dataRows`)
+        console.log(dataRows)
+        dataRows.shift()
+        dataRows.shift()
+        //dataRows.pop()
+        console.log(`dataRows 2nd removed`)
+        console.log(dataRows)
+        const surveyCSVData = convertSurveyJsonToCSV(finalHeaders, dataRows, rowsReales)
+        console.log(`surveyCSVData`)
+        console.log(surveyCSVData)
+        saveStringAsCSV(surveyCSVData, `${new Date().getTime()/1000}_data.csv`)
+  }
+  resolve(true)
+  })
+  
 }
-}
-
-
-   useEffect(() => {
-    objLogSettings.sourceFilename = 'UploadSurveyAnswers.js'
-    objLogSettings.sourceFunction = 'useEffect'
-    LogThis(objLogSettings, `START`)
-    LogThis(objLogSettings, `in useEffect cycle uploading=${uploading}; fileContentsReady=${fileContentsReady}`)
-        if(!uploading && fileContentsReady && !uploadingReales && fileContentsRealesReady)
-        {
-          LogThis(objLogSettings, `processing CSV uploading=${uploading}; fileContentsReady=${fileContentsReady}; uploadingReales=${uploadingReales}; fileContentsRealesReady=${fileContentsRealesReady}`)
-          processCSVData()
-      }
-      
-  }, [dispatch, fileContents, fileContentsReady, uploading, uploadingReales, fileContentsRealesReady])
   
   const uploadFileRealesHandler = async (e) => {
+    //setmyCSV(false)
     objLogSettings.sourceFilename = 'UploadSurveyAnswers.js'
     objLogSettings.sourceFunction = 'uploadFileRealesHandler'
     LogThis(objLogSettings, `START`)
@@ -230,6 +224,7 @@ const processCSVData = () => {
   }
     
   const uploadFileHandler = async (e) => {
+    //setmyCSV(true)
     const file = e.target.files[0]
     console.log(`uploadFileHandler start`)
     setUploading(true)
@@ -253,26 +248,43 @@ const processCSVData = () => {
     }
   }
 
-  
-
   const submitHandler = (e) => {
     e.preventDefault()
   }
+  
+  useEffect(() => {
+    objLogSettings.sourceFilename = 'UploadSurveyAnswers.js'
+    objLogSettings.sourceFunction = 'useEffect'
+    LogThis(objLogSettings, `START`)
+    LogThis(objLogSettings, `in useEffect cycle uploading=${uploading}; fileContentsReady=${fileContentsReady}`)
+        if(!uploading && fileContentsReady && !uploadingReales && fileContentsRealesReady)
+        {
+          LogThis(objLogSettings, `processing CSV uploading=${uploading}; fileContentsReady=${fileContentsReady}; uploadingReales=${uploadingReales}; fileContentsRealesReady=${fileContentsRealesReady}`)
+          console.log(`setting myCSV to true myCSV=${myCSV}`)
+          setmyCSV(true)
+          console.log(`setted myCSV to true myCSV=${myCSV}`)
+          (async () => {
+            await processCSVData()
+            setmyCSV(false)
+            console.log(`setted myCSV to false myCSV=${myCSV}`)
+          })()
+            // processCSVData().then(()=>{
+            // console.log(`setting myCSV to false myCSV=${myCSV}`)
+            // setmyCSV(false)
+            // console.log(`setted myCSV to false myCSV=${myCSV}`)
+          // })
+           
+      }
+      
+  }, [dispatch, fileContents, fileContentsReady, uploading, uploadingReales, fileContentsRealesReady])
 
 return (
     <>
-      {/* <Link to='/admin/productlistadmin' className='btn btn-light my-3'>
-        Go Back
-      </Link> */}
+    {console.log(`rerender happening myCSV=${myCSV}`)}
+    <h1> myCSV={ myCSV ? 'True' : 'False' } </h1>
+    {myCSV && <Loader />}
       <FormContainer>
         <h1>Subir archivo de respuestas de encuestas.</h1>
-        {/* {loadingUpdate && <Loader />}
-        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <Message variant='danger'>{error}</Message>
-        ) : ( */}
           <Form onSubmit={submitHandler}>
             <Form.Group controlId='nombreDeArchivoNumerico'>
               <Form.Label>Folder y nombre de archivo con datos numericos</Form.Label>
@@ -287,7 +299,6 @@ return (
                 className=''
                 onChange={uploadFileHandler}
               ></Form.Control>
-              {uploading && <Loader />}
             </Form.Group>
             <Form.Group controlId='nombreDeArchivoReales'>
               <Form.Label>Folder y nombre de archivo con datos reales</Form.Label>
@@ -302,14 +313,8 @@ return (
                 className=''
                 onChange={uploadFileRealesHandler}
               ></Form.Control>
-              {uploadingReales && <Loader />}
             </Form.Group>
-            {/* <br />
-            <Button type='submit' variant='primary'>
-              Update
-            </Button> */}
           </Form>
-        {/* )} */}
       </FormContainer>
     </>
   )
