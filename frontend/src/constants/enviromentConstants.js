5789b9dd41c5fc2dc8a5d8d91b711464ccdab615
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2023-10-22 23:22:26 -0400

    Loading answers working

--- frontend/src/constants/enviromentConstants.js
+++ frontend/src/constants/enviromentConstants.js
@@ -1,19 +1,19 @@
 /** @format */
 
 // LOCALHOST DEV VARIABLES
-/*
-export const LOG_LEVEL = 1
-export const CURRENT_VERSION = `v1.0.0.7-2023-08-27-01:59`
-export const BACKEND_ENDPOINT = 'http://localhost:5000'
-export const KUARSIS_PUBLIC_STATIC_FOLDER = 'http://localhost:3000/images'
+
+export const LOG_LEVEL = 1;
+export const CURRENT_VERSION = `v1.0.0.7-2023-08-27-01:59`;
+export const BACKEND_ENDPOINT = "http://localhost:5000";
+export const KUARSIS_PUBLIC_STATIC_FOLDER = "http://localhost:3000/images";
 // export const KUARSIS_PUBLIC_STATIC_IMG_FOLDER = 'http://localhost:3000/img'
 // export const KUARSIS_PUBLIC_STATIC_MODELS_FOLDER = 'http://localhost:3000/models'
-export const KUARSIS_BANNER_MAIN_LOGO = '/ArtPixanLogo256px.png'
+export const KUARSIS_BANNER_MAIN_LOGO = "/ArtPixanLogo256px.png";
 export const KUARSIS_PUBLIC_BUCKET_URL =
-  'https://kuarsis-products-s3-public-dev.s3.amazonaws.com/'
-*/
-//CONSTANTS FOR ARTPIXANDEV.KUARXIS.COM (DEV) ENVIRONMENT
+  "https://kuarsis-products-s3-public-dev.s3.amazonaws.com/";
 
+//CONSTANTS FOR ARTPIXANDEV.KUARXIS.COM (DEV) ENVIRONMENT
+/*
 export const LOG_LEVEL = 1;
 export const CURRENT_VERSION = `v1.0.0.7-2023-08-29-20:22`;
 export const BACKEND_ENDPOINT =
@@ -23,7 +23,7 @@ export const KUARSIS_PUBLIC_STATIC_FOLDER =
 export const KUARSIS_BANNER_MAIN_LOGO = "ArtPixanLogo256px.png";
 export const KUARSIS_PUBLIC_BUCKET_URL =
   "https://kuarsis-products-s3-public-dev.s3.amazonaws.com/";
-
+*/
 //CONSTANTS FOR ARTPIXAN.KUARXIS.COM (PROD) ENVIRONMENT
 /*
 export const LOG_LEVEL = 0
