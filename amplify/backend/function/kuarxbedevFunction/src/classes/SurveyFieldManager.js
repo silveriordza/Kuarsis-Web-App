d8085a0798fa1767840bc7aad1f93c2cdde9af3c
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2024-01-18 01:06:59 -0500

    Intermediate commit to backup local changes

--- amplify/backend/function/kuarxbedevFunction/src/classes/SurveyFieldManager.js
+++ amplify/backend/function/kuarxbedevFunction/src/classes/SurveyFieldManager.js
@@ -1,45 +1,54 @@
-const TemplateManager = require("./TemplateManager")
-const { LogManager, L3} = require("./LogManager.js")
-const { addPropertyMatchingValueInArray } = require("../utils/Functions.js")
+/** @format */
 
-const sourceFilename = "SurveyFieldManager.js"
+const TemplateManager = require('./TemplateManager')
+const { LogManager, L3 } = require('./LogManager.js')
+const { addPropertyMatchingValueInArray } = require('../utils/Functions.js')
+
+const sourceFilename = 'SurveyFieldManager.js'
 
 class SurveyFieldManager extends TemplateManager {
-    constructor(collectionName, identifierFieldName, templateFieldToLink, referencedLinkField, linkedCollection, externalLinkField){
-        super(     
-            //templateList,
-            collectionName,
-            identifierFieldName
-            )
-            this.preProcessTemplate = this.preProcessTemplate.bind(this); // Bind A to the parent class
-            this.log = new LogManager (sourceFilename, "constructor")
-            this.log.LogThis(`START`, L3)
-            this.templateFieldToLink = templateFieldToLink
-            this.referencedLinkField = referencedLinkField
-            this.linkedCollection = linkedCollection
-            this.externalLinkField = externalLinkField
-        }
-        
-     //Updates the namesList in the parent TemplateMangaer based on specific function for the type of template.
-    preProcessTemplate () {
-        addPropertyMatchingValueInArray(
-            this.templateList,
-            this.referencedLinkField,
-            this.templateFieldToLink,
-            this.externalLinkField,
-            this.collectionToLink)
-    }
+   constructor(
+      collectionName,
+      identifierFieldName,
+      templateFieldToLink,
+      referencedLinkField,
+      linkedCollection,
+      externalLinkField,
+   ) {
+      super(
+         //templateList,
+         collectionName,
+         identifierFieldName,
+      )
+      this.preProcessTemplate = this.preProcessTemplate.bind(this) // Bind A to the parent class
+      this.log = new LogManager(sourceFilename, 'constructor')
+      this.log.LogThis(`START`, L3)
+      this.templateFieldToLink = templateFieldToLink
+      this.referencedLinkField = referencedLinkField
+      this.linkedCollection = linkedCollection
+      this.externalLinkField = externalLinkField
+   }
 
-    async save(templateList, collectionToLInk){
-        this.log.setFunctionName('save')
-        this.templateList= templateList
-        this.collectionToLink = collectionToLInk
-        this.preProcessTemplate()
-        //If I don't add the call like below with prototype and the name of the class, there was an issue because the child class of SurveyFieldManager is SurveyMiltiManager and both share the function preProcessTemplate with the same name and when the child invokes save of the parent then the parent was calling the child's preProcessTemplate if invoked form the parent with this.preProcessTemplate. To fix that I added theis explicit invokation.
-        SurveyFieldManager.prototype.preProcessTemplate.call(this);
-        return await super.save()
-    }
-    
+   //Updates the namesList in the parent TemplateMangaer based on specific function for the type of template.
+   preProcessTemplate() {
+      addPropertyMatchingValueInArray(
+         this.templateList,
+         this.referencedLinkField,
+         this.templateFieldToLink,
+         this.externalLinkField,
+         this.collectionToLink,
+      )
+   }
+
+   async save(templateList, collectionToLInk) {
+      this.log.setFunctionName('save')
+      this.templateList = templateList
+      this.collectionToLink = collectionToLInk
+      this.preProcessTemplate()
+      //If I don't add the call like below with prototype and the name of the class, there was an issue because the child class of SurveyFieldManager is SurveyMiltiManager and both share the function preProcessTemplate with the same name and when the child invokes save of the parent then the parent was calling the child's preProcessTemplate if invoked form the parent with this.preProcessTemplate. To fix that I added theis explicit invokation.
+      SurveyFieldManager.prototype.preProcessTemplate.call(this)
+      return await super.save()
+   }
 }
 
 module.exports = SurveyFieldManager
