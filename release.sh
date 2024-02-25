c461f275dff990476df8bc57a90cc94c6303f122
Silverio Rodriguez Alcorta <silverio.rodriguez@kuarxis.com>
2024-02-25 17:04:03 -0500

    Intermediate commit to backup local changes

--- release.sh
+++ release.sh
@@ -4,6 +4,8 @@ source checkYesNo.sh
 
 question="We are about to release, commit tag code into git."
 
+#The below is the way to call a function from the bash checkYesNo.sh file passing a parameter.
+#Remeber to enclose the parameter in "" otherwise it may cause problems when reading the parameter when spaces are in the text. 
 result=$(checkYesNo "$question")
 returnedValue=$?
 echo " The return value is: $returnedValue"
@@ -11,6 +13,9 @@ echo " The return value is: $returnedValue"
 
 if [ "$returnedValue" -ge 2 ]; then
 echo "$result"
+# If you use exit 1 to stop the execution, the whole terminal will close.
+#exit 1
+# If you want the bash to stop execution without exiting terminal just do a return. The return value can be read with $?
 return 1
 fi
 
@@ -19,7 +24,7 @@ comment="$2"
 
 
 git add .
-git commit -m 'Intermediate commig to backup local changes'
+git commit -m 'Intermediate commit to backup local changes'
 
 source commitwmsh.sh
 # localBranchName=$(git branch --show-current)
